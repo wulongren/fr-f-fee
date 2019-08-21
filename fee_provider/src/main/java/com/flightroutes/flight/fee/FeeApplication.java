643e62fb97267888533965fcec788acd49081e8a
王雷 <master@wangleideMacBook-Pro.local>
2019-08-21 23:23:59 +0800

    fix

--- fee_provider/src/main/java/com/flightroutes/flight/fee/FeeApplication.java
+++ fee_provider/src/main/java/com/flightroutes/flight/fee/FeeApplication.java
@@ -1,15 +1,11 @@
 package com.flightroutes.flight.fee;
 
-import org.apache.dubbo.config.spring.context.annotation.DubboComponentScan;
-import org.apache.dubbo.config.spring.context.annotation.EnableDubboConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ImportResource;
 
-@EnableDubboConfig
 @SpringBootApplication
-@DubboComponentScan("com.flightroutes.flight.fee.service.FrServiceFeeService")
-@ImportResource("classpath:root.xml,dubbo/dubbo-expose.xml")
+@ImportResource(value = {"classpath:root.xml", "classpath:dubbo/dubbo-expose.xml"})
 public class FeeApplication {
 
     public static void main(String[] args) {
