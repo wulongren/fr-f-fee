a053f984d76c165d2d2b2df7159670d75bee239e
lei.wang <lei.wang@flightroutes24.com>
2019-08-22 20:22:06 +0800

    修改依赖关系

--- fee_provider/src/main/java/com/flightroutes/flight/fee/FeeApplication.java
+++ fee_provider/src/main/java/com/flightroutes/flight/fee/FeeApplication.java
@@ -2,9 +2,10 @@ package com.flightroutes.flight.fee;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.ImportResource;
 
-@SpringBootApplication
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
 @ImportResource(value = {"classpath:root.xml"})
 public class FeeApplication {
 
