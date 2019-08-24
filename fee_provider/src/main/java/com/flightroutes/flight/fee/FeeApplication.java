5803ec64e52a5c76acbb437fd37f554d28957c0f
王雷 <master@wangleideMacBook-Pro.local>
2019-08-24 08:00:58 +0800

    edit package for jar

--- fee_provider/src/main/java/com/flightroutes/flight/fee/FeeApplication.java
+++ fee_provider/src/main/java/com/flightroutes/flight/fee/FeeApplication.java
@@ -3,18 +3,11 @@ package com.flightroutes.flight.fee;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 @ImportResource(value = {"classpath:root.xml"})
-public class FeeApplication extends SpringBootServletInitializer {
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(FeeApplication.class);
-    }
-
+public class FeeApplication {
     public static void main(String[] args) {
         SpringApplication.run(FeeApplication.class, args);
     }
