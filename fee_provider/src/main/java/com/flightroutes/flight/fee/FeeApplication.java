e113a97ac164d7beefd6bd05fe0ddee1470d91e4
ting.li <ting.li@flightroutes24.com>
2019-10-15 15:40:41 +0800

    修改配置文件

--- fee_provider/src/main/java/com/flightroutes/flight/fee/FeeApplication.java
+++ fee_provider/src/main/java/com/flightroutes/flight/fee/FeeApplication.java
@@ -4,12 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.ImportResource;
+import tk.mybatis.spring.annotation.MapperScan;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@SpringBootApplication
+@MapperScan(basePackages = "com.flightroutes.flight.fee.repository")
 @ImportResource(value = {"classpath:root.xml"})
 public class FeeApplication {
     public static void main(String[] args) {
         SpringApplication.run(FeeApplication.class, args);
     }
-
 }
