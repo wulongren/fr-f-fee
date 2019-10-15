99be4772de67b59a7ee842d3f05669aa2eb24c36
ting.li <ting.li@flightroutes24.com>
2019-10-15 19:37:37 +0800

    update

--- fee_api/src/main/java/com/flightroutes/flight/fee/api/service/ServiceFeeApiService.java
+++ fee_api/src/main/java/com/flightroutes/flight/fee/api/service/ServiceFeeApiService.java
@@ -2,9 +2,7 @@ package com.flightroutes.flight.fee.api.service;
 
 import com.flightroutes.flight.comm.biz.RetData;
 import com.flightroutes.flight.fee.api.bean.ServiceFeeRs;
-
 public interface ServiceFeeApiService {
-	
 	
 	RetData<ServiceFeeRs> getServiceFee(String purchaserCode);
 
